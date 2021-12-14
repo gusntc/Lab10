9b4a2e262032a2f77e87afbf4608d4f5ecdd807d
gusntc <Natacha_rungbanpant@cmu.ac.th>
2021-12-14 16:30:10 +0700

    Lab10_1Edit

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,14 +2,16 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+	double principle,interest,canafford;	
 	cout << "Enter initial loan: ";
+	cin >> principle;
 	cout << "Enter interest rate per year (%): ";
+	cin >> interest;
 	cout << "Enter amount you can pay per year: ";
+	cin >> canafford;
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
+	
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -18,16 +20,33 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	
+	for(int i = 1;true;i++){
+	double DB = principle*(interest/100);
+	double Total = principle+DB;
+	double NewBalance = Total-canafford;
+		if(principle <= canafford){
+			cout << fixed << setprecision(2); 
+			cout << setw(13) << left << i; 
+			cout << setw(13) << left << principle;
+			cout << setw(13) << left << DB;
+			cout << setw(13) << left << Total;
+			cout << setw(13) << left << Total;
+			cout << setw(13) << left << Total-Total;
+			cout << "\n";
+			break;
+		}
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << i; 
+		cout << setw(13) << left << principle;
+		cout << setw(13) << left << DB;
+		cout << setw(13) << left << Total;
+		cout << setw(13) << left << canafford;
+		cout << setw(13) << left << NewBalance;
+		cout << "\n";
+		principle = NewBalance;
+		
+	}
 	
 	return 0;
 }
